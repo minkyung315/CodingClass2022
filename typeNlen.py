be2de6827b5229f3444fcce9decfd01aea3a62bc
minkyung315 <c.minkyungk@gmail.com>
2022-01-31 13:21:13 +0000

    typeNlen file

---
+++ typeNlen.py
@@ -0,0 +1,5 @@
+print(type("안녕하세요")) #str = string의 약자, 문자열 의미
+print(type(273)) #int = integer의 약자, 정수 의미
+print(type(342.64)) #float, 실수 의미
+print("\n") #줄바꿈
+print(len("안녕하세요")) #문자열의 길이 구할 때 사용되는 함수
