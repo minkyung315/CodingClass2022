62c8608916a7a90881e61255084e2eb79728a544
minkyung315 <c.minkyungk@gmail.com>
2022-02-03 08:23:17 +0000

    if file

---
+++ if.py
@@ -0,0 +1,15 @@
+#입력값을 받습니다.
+number = input("정수 입력 >")
+number = int(number)
+
+#양수 조건
+if number > 0:
+  print("양수 입니다.")
+
+#음수 조건
+if number < 0:
+  print("음수 입니다.")
+
+#0 조건
+if number == 0:
+  print("0입니다.")
