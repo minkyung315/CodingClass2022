10b62fdaf81037059c8a9fb8a853ee2b0e7fcaac
minkyung315 <c.minkyungk@gmail.com>
2022-02-10 08:36:26 +0000

    for file

---
+++ for.py
@@ -0,0 +1,14 @@
+for i in range(10):
+  print("출력")
+
+#리스트를 선언합니다.
+array = [273,32,103,57,52]
+
+#리스트에 반복문을 적용합니다.
+for element in array:
+  #출력합니다.
+  print(element)
+#for 반복문은 리스트에 있는 요소 하나하나가 element라는 변수에 들어가며, 차례차례 반복됨.
+
+for character in "Hello": #문자열을 for 반복문의 뒤에 넣으면 글자 하나하나에 반복이 적용된다.
+  print(character)
